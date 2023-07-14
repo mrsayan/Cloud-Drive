b5cb31a861e5e10aec9c2074e4471ab95cef7524
mrsayan <iamsayan568@gmail.com>
2023-07-14 13:18:34 +0530

    Initial commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect("process.env.MONGO_URI || mongodb://localhost:27017/cloudManager", {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connection established"));
