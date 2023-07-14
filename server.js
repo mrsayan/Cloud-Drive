683fd1654437b5cc5341ccf2ade8c3880ed0b31d
mrsayan <iamsayan568@gmail.com>
2023-07-14 13:25:57 +0530

    Initial commit

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv/config");
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("process.env.MONGO_URI || mongodb://localhost:27017/cloudManager", {
+  .set('strictQuery', false)
+  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/template", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
